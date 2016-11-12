64d1f80119fcfe8d7b70766a4a9180f8d81a6925
k3time <kuldeepkaushikdelhi@gmail.com>
2016-11-12 14:03:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var session = require('express-session');
+var hbars = require('express-handlebars');
+var chalk = require('chalk');
+var db = require('./models/db.js'); // db.js must be required before routes.js
+var app = module.exports = express(); // exporting apps must be done before routes.js
+var routes = require('./routes/routes.js');
+var net = require('net');
+
+app.use(express.static(__dirname + "/"));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+    extended: false
+}));
+app.use(session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: false
+}));
+app.set('view engine', 'handlebars');
+app.engine('handlebars', hbars({
+    defaultLayout: 'layout'
+}));
+
+app.get('/', routes.loginPageHandler);
+app.get('/logout', routes.logoutPageHandler);
+app.post('/auth', routes.authHandler);
+app.get('/console', routes.consoleHandler);
+app.get('/registerForm', routes.registerFormHandler);
+app.post('/register', routes.registerUserHandler);
+app.get('/edit', routes.editPageHandler);
+app.post('/saveChanges', routes.saveChangesHandler);
+app.get('/delete', routes.deletePageHandler);
+app.get('/addForm', routes.addFormHandler);
+app.post('/add', routes.addHandler);
+
+//error handling
+app.use("*", function(req, res) {
+    res.status(404);
+    res.render('message.handlebars', {
+        message: '<blockquote class="mainLines"><code> The page you are looking for is not available or may have been moved.</code> </blockquote>'
+    });
+});
+
+app.use(function(error, req, res, next) {
+    console.log(chalk.red('Error : 500::' + error))
+    res.status(500);
+    res.render('message.handlebars', {
+        message: '<blockquote class="mainLines"><code>something went wrong as you tried to access this page</code>Probably this happened because there are some bugs in the application</blockquote>'
+    });
+});
+
+var port = process.env.PORT || 3333;
+
+console.log("Checking the availability of port %d", port);
+var netServer = net.createServer();
+netServer.once('error', function(err) {
+    if (err.code === 'EADDRINUSE') {
+        console.log(chalk.red("port %d is currently in use", port));
+        return;
+    }
+});
+
+netServer.listen(port, function() {
+    console.log(chalk.green('Net server is able to listen on port: ' + port));
+    netServer.close();
+    console.log(chalk.green('Closing Net server on port: ' + port));
+
+    app.listen(port, function() {
+        console.log(chalk.green("Http server is listening on port [" + port + '] '));
+    });
+});
---
+++ routes/routes.js
@@ -0,0 +1,160 @@
+var mongoose = require( 'mongoose' );
+var UserModel = mongoose.model( 'UserModel' );
+var TechModel = mongoose.model( 'TechModel' );
+var chalk = require('chalk');
+var app = require('../app');//expressJS allows circular dependencies
+
+var gLoggedIN = 0;
+
+exports.loginPageHandler = function (req, res){
+	res.render('login.handlebars', {LoggedIN: gLoggedIN});
+};//loginPageHandler
+
+exports.logoutPageHandler = function (req, res){
+  req.session.destroy();
+  gLoggedIN = 0;  
+  res.render('message.handlebars', {message:'<span class="label label-success">You have logged-out successfully</span>', 
+                                   LoggedIN: gLoggedIN});
+};//logoutPageHandler
+
+exports.authHandler = function (req, res){
+	var nmReq = req.body.nm;
+	var pwdReq = req.body.pwd;
+	var authResult;
+
+	UserModel.findOne({username:nmReq}, function(err, userObj){
+    if(userObj === null){
+     	authResult = '<span class="label label-danger">Login Failed: User name does not exist in db</span>' ;
+      res.render('message.handlebars', {message:authResult, 
+                                        LoggedIN: gLoggedIN
+                                        });
+    } else if (pwdReq === userObj.password){
+				authResult =   '<span class="label label-success">Login successful</span>' ;   
+        gLoggedIN = 1;
+        
+        req.url = '/console';
+        req.method = 'get';
+        app._router.handle(req, res);
+        // res.render('message.handlebars', {message:authResult,
+        //                                  LoggedIN: gLoggedIN
+        //                                 });
+	  } else{
+				authResult = '<span class="label label-danger">Login Failed: Password did not match</span>' ; 
+        res.render('message.handlebars', {message:authResult, 
+                                          LoggedIN: gLoggedIN});
+	  }
+		console.log("Login Name %s, Password %s. Login outcome [%s]", nmReq, pwdReq, authResult);
+	});//UserModel.findOne
+}; //authHandler
+
+
+exports.consoleHandler = function (req, res){
+  if (gLoggedIN){}else{
+    res.status(500);
+    res.send("Incorrect request");
+    return;
+  }
+  
+  var recordsArray; //to keep all tech records
+  TechModel.find({}, function(err, techArray){
+    if (!err){
+      recordsArray = techArray;
+      outcomeBoolean = 1;
+      gLoggedIN = 1;
+      res.render('console.handlebars', {recordsArray:recordsArray,
+                                        LoggedIN: gLoggedIN
+                                       });
+      } 
+  }); //TechModel.find       
+}; //consoleHandler
+
+
+
+exports.registerFormHandler = function(req, res){
+   res.render("register.handlebars", {LoggedIN: gLoggedIN});
+}; //registerFormHandler
+
+exports.registerUserHandler = function(req, res){
+   var newuser = new UserModel();
+   newuser.username = req.body.username;;
+   newuser.email = req.body.email;
+   newuser.password = req.body.password;
+
+   //save to db through model :: Add a record
+   newuser.save(function(err, savedUser){
+     if(err){
+       var message = "A user already exists with that username or email";
+       console.log(message);
+       res.render("register.handlebars", {errorMessage:message, 
+                                          LoggedIN: gLoggedIN});
+     }else{
+       req.session.newuser = savedUser.username;
+       res.render('message.handlebars', {message:'<span class="label label-success">Registration successful</span>', 
+                                         LoggedIN: gLoggedIN});
+     }
+   }); //newuser.save
+};//registerUserHandler
+
+exports.editPageHandler = function(req, res){
+  var techToEdit = req.query.tech;
+  TechModel.findOne({tech:techToEdit}, function(err, techRec){
+  if (!err){
+    console.log(chalk.yellow("Going to edit -> [" + techRec.tech + " : " + techRec.description + "]"));
+    res.render('editPage.handlebars', {techRec: techRec, LoggedIN: gLoggedIN});
+  } 
+}); //TechModel.findOne
+}; //editPageHandler
+
+exports.saveChangesHandler = function(req, res){
+  var techRequest = req.body.techname;
+  var techDescrRequest = req.body.techdescr;
+  //console.log("Saving Edited records : " + techRequest + " : " + techDescrRequest);
+  var message;
+  //update rec through model
+  TechModel.update({tech:techRequest}, 
+                    {$set: { description: techDescrRequest }}, 
+                    {multi:false}, function(err, updatedRec){
+   if(err){
+     message = '<span class="label label-danger">Update Failed</span>';
+     console.log(chalk.red(message));
+     res.render('message.handlebars', {message: message, LoggedIN: gLoggedIN});
+   }else{
+     message = '<span class="label label-success">A record saved succesfully</span>';
+     console.log(chalk.green(message));
+     res.render('message.handlebars', {message: message, LoggedIN: gLoggedIN});
+   }
+  });
+}; //saveChangesHandler
+
+exports.deletePageHandler = function(req, res){
+  var techToEdit = req.query.tech;
+  TechModel.remove({tech:techToEdit}, function(err, techRec){
+  if (!err){
+    var message = '<span class="label label-success">A record removed successfully</span>'
+    res.render('message.handlebars', {message: message, LoggedIN: gLoggedIN});
+  } 
+}); //TechModel.remove
+}; //editPageHandler
+
+exports.addFormHandler = function(req, res){
+    res.render('add.handlebars', {LoggedIN: gLoggedIN});
+ }; //addFormHandler
+
+exports.addHandler = function(req, res){
+  var message;
+  var newTech = new TechModel();
+  newTech.tech = req.body.techname;
+  newTech.description = req.body.techdescr;
+   //save to db through model :: Add a record
+   newTech.save(function(err, savedUser){
+     if(err){
+       message = '<span class="label label-danger">A record already exists with given technology</span>';
+       console.log(message);
+       res.render("message.handlebars", {message:message, LoggedIN: gLoggedIN});
+     }else{
+       message = '<span class="label label-success">A new technology added successfully</span>';
+       res.render('message.handlebars', {message:message, LoggedIN: gLoggedIN});
+     }
+   }); //newTech.save
+}; //addHandler
+
